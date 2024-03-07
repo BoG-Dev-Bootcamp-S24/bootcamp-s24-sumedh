8d049aeb18680539cc4ff320b34004ce063b9e73
sdurvasula8 <sdurvasula8@gatech.edu>
2024-03-07 18:20:31 -0500

    lecture9

--- demo/lecture9/my-app/pages/api/pokemon.js
+++ demo/lecture9/my-app/pages/api/pokemon.js
@@ -1,25 +1,25 @@
-const pokemonData = {}
+const pokemondata = {}
 
 export default async function handler(req, res) {
-  const name = req.query.name;
-  const url = "https://pokeapi.co/api/v2/pokemon/";
+    const name = req.query.name;
+    const url = "https://pokeapi.co/api/v2/pokemon/";
 
-  if (req.method === 'GET') {
-    if(!name) {
-      res.status(400).json({error : "name is required"});
+    if (req.method === 'GET') {
+        if (!name) {
+            res.status(400).json({error : "no name given"});
+        }
+        if (name) {
+            const callURL = url + name;
+            const response = await fetch(callURL);
+            const data = await response.json();
+            res.status(200).json(data);
+        } else {
+            const data = pokemondata[name];
+            res.status(200).json(data);
+        }
+    }  else if (req.method === 'POST') {
+        const data = req.query.data;
+        pokemondata[name] = data;
+        res.status(200).json({ name });
     }
-    if (pokemonData[name]) {
-      res.status(200).json(pokemonData[name]);
-    }
-    if (name) {
-      const callURL = url + name;
-      const response = await fetch(callURL);
-      const data = await response.json();
-      res.status(200).json(data);
-    } 
-  } else if (req.method === 'PUT') {
-    const data = req.query.data;
-    pokemonData[name] = data;
-    res.status(200).json({ message: `data overwritten for ${name}`});
-  }
 }
